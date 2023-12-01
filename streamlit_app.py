d2ca14ac9214d8c5b546ec7fbbae2be0bc91a8d9
alminayuwon <152366752+alminayuwon@users.noreply.github.com>
2023-12-01 14:56:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
-import streamlit as st
-st.write('Hello World!')
+import streamlit as streamlit
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+st.subheader("Hi, I am Minari :wave:")
+st.title("")
