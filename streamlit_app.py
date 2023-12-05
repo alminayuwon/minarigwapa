e0ebb1973bfde311c26a4b304c3ba30fa48729f3
alminayuwon <152366752+alminayuwon@users.noreply.github.com>
2023-12-05 14:04:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,11 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Multipage App",
-    page_icon="",
-)
-
-st.title("Main Page")
-st.sidebar.success("Select a page above.")
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 # ---- HEADER SECTION ----
 with st.container():
- st.subheader("Hi I am Reign Heinz")
+ st.subheader("Hi I am Almina")
  st.title("A Students from SNSU")
  st.write("I am passionate to learn from this amazing school")
 
