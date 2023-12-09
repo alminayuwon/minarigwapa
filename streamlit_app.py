df93106d1a39ef14e9da3421e3229f3e0f785cf6
alminayuwon <152366752+alminayuwon@users.noreply.github.com>
2023-12-09 14:23:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 # ---- HEADER SECTION ----
 with st.container():
- st.subheader("Hey,I'm Almina Adlawon, an 18-year-old on a  mission to decode the lanuage of computers.")
+ st.subheader("Hey,I'm Almina Adlawon, an 18-year-old")
  st.title("Computer Engineering student at SNSU")
  st.write("Passionate about turning ideas into code!")
 
